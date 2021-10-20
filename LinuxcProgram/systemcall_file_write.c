2038a9f78289bf0143cbb003988e6260d0e1e98c
hhdowload <youth1212@163.com>
2021-10-20 23:53:01 +0800

    write系统调用

---
+++ LinuxcProgram/systemcall_file_write.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+int main ()
+{
+  if ((write(1, "Here is some data\n",18 )) !=18 )   
+    write(2,"A write error has occured on file descriptor 1\n",46 );
+
+exit(0);
+}
