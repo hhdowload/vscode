67a9b6c5ce9882c34f50358861a7c295bc31b3e5
hhdowload <youth1212@163.com>
2021-10-24 01:03:54 +0800

    get0pt 函数

---
+++ LinuxcProgram/env_getopt.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+
+int main(int argc, char *argv[])
+{
+    int opt;
+    while( (opt = getopt(argc,argv[],":if:lr")) == -1 ) {
+        switch (opt)
+        {
+        case i:
+        case l:
+        case r:
+            printf("option: %c\n", opt);
+            break;
+        case f:
+            printf("filename is %s\n", optarg);
+            break;
+        case ':':
+            printf("option neeed avalue\n");
+            break;
+        case '?':
+            printf("got a unkonw option")
+        default:
+            break;
+        }
+    }
+    for(; optind < argc; optind++ )
+    {
+        printf("argument is %s\n", argv[optind]);
+    }
+
+    exit(0);
+}
